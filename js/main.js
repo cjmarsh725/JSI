9a1fa2d6543bb813da00fac27d09160ea735873b
Chris <cjmarsh725@gmail.com>
2018-04-03 07:11:06 -0700

    Refactored header buttons

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const syllabusBtn = document.querySelector('.syllabus-btn');
+syllabusBtn.addEventListener('click', (event) => {
+  window.open('https://tk.lambdaschool.com/cs-master','new_window');
+});
+
+const videosBtn = document.querySelector('.videos-btn');
+videosBtn.addEventListener('click', (event) => {
+  window.open('https://www.youtube.com/watch?v=IPMvxZR9Me8','new_window');
+});
